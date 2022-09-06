a27b70813ab868e80cdf3561313a10fedb88918d
ArthurCarvalho15 <arthur.demario@gmail.com>
2022-09-06 17:50:31 -0300

    :tada: Primeiro Commit

---
+++ src/App.tsx
@@ -0,0 +1,66 @@
+import { Header } from "./components/Header";
+import { Post } from "./components/Post";
+import { Sidebar } from "./components/Sidebar";
+
+import styles from './App.module.css'
+
+import './global.css';
+
+// author: { avatar_url: "", name: "", role: "" }
+// publishedAt: Date
+// content: String
+
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: 'https://github.com/ArthurCarvalho15.png',
+      name: 'Arthur Carvalho',
+      role: 'Fullstack Developer'
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galeraa 👋'},
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀'},
+      { type: 'link', content: 'jane.design/doctorcare'}
+    ],
+    publishedAt: new Date('2022-08-31 17:14:00'),
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: 'https://github.com/FortunaArthur.png',
+      name: 'Arthur Sanchez',
+      role: 'Python Developer'
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galeraa 👋'},
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀'},
+      { type: 'link', content: 'jane.design/doctorcare'}
+    ],
+    publishedAt: new Date('2022-09-01 17:14:00'),
+  },
+];
+
+export function App() {
+  return (
+    <div>
+      <Header/>
+      
+      <div className={styles.wrapper}>
+        <Sidebar/>
+        <main>
+          {posts.map(post => {
+            return (
+              <Post
+                key={post.id}
+                author = {post.author}
+                content = {post.content}
+                publishedAt = {post.publishedAt}
+              />
+            )
+          })}
+        </main>
+      </div>
+    </div>
+  )
+}
